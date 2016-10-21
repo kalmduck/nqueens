655e070a5aab96837cb30bef21f999f33bd16199
Ben Strecker <benstrecker@gmail.com>
2016-10-21 09:22:04 -0400

    Adding documentation

--- nqueens.go
+++ nqueens.go
@@ -13,6 +13,8 @@ var n int
 // the queen in that row occupies.
 type Board []int
 
+// posToSquare sweeps away some of the mess caused by
+// using the interface.
 func posToSquare(pos backtrack.Position) Square {
 	s, ok := pos.(Square)
 	if !ok {
@@ -21,6 +23,9 @@ func posToSquare(pos backtrack.Position) Square {
 	return s
 }
 
+// String converts the current state of the the Board to a string.
+// The position of a queen is marked with a 'Q', and empty squares are
+// marked with a '+'.
 func (b Board) String() string {
 	var s string
 	s += "\n"
@@ -39,6 +44,10 @@ func (b Board) String() string {
 	return s
 }
 
+// Valid checks if the given position is a valid place for a queen given
+// the current board state. Returns false if the square is already covered by
+// a queen on the board.  Assumes that queens have only been placed in
+// previous rows.
 func (b Board) Valid(pos backtrack.Position) bool {
 	s := posToSquare(pos)
 	for i := 0; i < s.r; i++ { // for every row that has a queen so far
@@ -55,44 +64,59 @@ func (b Board) Valid(pos backtrack.Position) bool {
 
 func checkDiag(qr, qc, r, c int) bool {
 	var rowDiff, colDiff int
-	if rowDiff = r - qr; rowDiff < 0 {
+	if rowDiff = r - qr; rowDiff < 0 { // abs
 		rowDiff = -rowDiff
 	}
-	if colDiff = c - qc; colDiff < 0 {
+	if colDiff = c - qc; colDiff < 0 { // abs
 		colDiff = -colDiff
 	}
 	return rowDiff == colDiff
 }
 
+// Record marks this position with a queen.
+// The column choices range from 1 - n, not the typical
+// zero offset.
 func (b Board) Record(pos backtrack.Position) {
 	s := posToSquare(pos)
 	b[s.r] = s.c + 1
 }
 
+// Done returns true if we have just placed the n-th queen, i.e.
+// all the queens are placed, and we have found a solution.
 func (b Board) Done(pos backtrack.Position) bool {
 	s := posToSquare(pos)
 	return s.r == n-1
 }
 
+// Undo removes a queen from the board at the passed position.
+// This is accomplished by marking that position as 0 again.
 func (b Board) Undo(pos backtrack.Position) {
 	s := posToSquare(pos)
 	b[s.r] = 0
 }
 
+// Square represents a particular location on the chess board grid
 type Square struct {
 	r, c int
 }
 
+// NextVal returns a Position of the next column value in the
+// current row.
 func (s Square) NextVal() backtrack.Position {
 	var p backtrack.Position
 	p = Square{s.r, s.c + 1}
 	return p
 }
 
+// End returns true if the Square is at the end of all
+// Possible columns.
 func (s Square) End() bool {
 	return !(s.c < n)
 }
 
+// NextPos advances to the next row in the chess board. In the algorithm,
+// this is called after placing a queen in the current row to
+// move on to the beginning of the next row
 func (s Square) NextPos() backtrack.Position {
 	var p backtrack.Position
 	p = Square{s.r + 1, 0}
